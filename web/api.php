6e71b07eff943d9fa82f97b2d7d75b99879ff8b7
Taylor Thurlow <taylorthurlow8@gmail.com>
2015-10-10 08:41:14 -0700

    ajax sux
    shits still broken

--- web/api.php
+++ web/api.php
@@ -1,30 +1,22 @@
 <?php
+	$con = mysqli_connect("127.0.0.1", "tsk", "asswipe#####", "powerplay");
+	if(mysqli_connect_errno($con)) {
+		error.log("Failed to connect to MySQL database. Please contact a network admin. " . mysqli_connect_error());
+	}
+	session_start();
 
-  //--------------------------------------------------------------------------
-  // Example php script for fetching data from mysql database
-  //--------------------------------------------------------------------------
-  $host = "localhost";
-  $user = "tsk";
-  $pass = "asswipe#####";
+	$result = mysqli_query($con, "SELECT COUNT(*) FROM `rooms`");
+	$data = mysqli_fetch_array($result);
+	$numRows = $data[0];
 
-  $databaseName = "powerplay";
-  $tableName = "rooms";
+	for ($i = 0; $i <= $numRows; $i++) {
+		$query = "SELECT * FROM `rooms` WHERE `id` = $i";
+		$result = mysqli_query($con, $query);
+		$data = mysqli_fetch_array($result);
+		$printNickname = $data['nick'];
+		$_POST['nick'] = $printNickname;
+		echo $_POST['nick'];
+	}
 
-  //--------------------------------------------------------------------------
-  // 1) Connect to mysql database
-  //--------------------------------------------------------------------------
-  $con = mysql_connect($host,$user,$pass);
-  $dbs = mysql_select_db($databaseName, $con);
-
-  //--------------------------------------------------------------------------
-  // 2) Query database for data
-  //--------------------------------------------------------------------------
-  $result = mysql_query("SELECT * FROM $tableName");          //query
-  $array = mysql_fetch_row($result);                          //fetch result
-
-  //--------------------------------------------------------------------------
-  // 3) echo result as json
-  //--------------------------------------------------------------------------
-  echo json_encode($array);
-
+	mysqli_close($con);
 ?>
